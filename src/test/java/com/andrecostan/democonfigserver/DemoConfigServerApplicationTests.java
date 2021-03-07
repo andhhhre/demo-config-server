c1c915197a85d13df7af3de4ca5f82f316443cfe
André Costa <andhhhre@gmail.com>
2021-03-06 21:49:12 -0300

    first commit

---
+++ src/test/java/com/andrecostan/democonfigserver/DemoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrecostan.democonfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
